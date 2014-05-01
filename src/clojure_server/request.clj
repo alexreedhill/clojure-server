5afe483e58117f8b5ca816b2aea3fab2da5b87a6
alexreedhill <ahill1994@student.elgin.edu>
2014-05-01 15:54:46 -0500

    Use same function to parse all params whether from query string or headers

--- src/clojure_server/request.clj
+++ src/clojure_server/request.clj
@@ -1,12 +1,12 @@
 (ns clojure-server.request)
 (use '[clojure.string :only (split)])
 
-(defn parse-params [query-string]
+(defn parse-params [params outer-delimiter inner-delimiter]
   (loop [acc          {}
-         split-params (split query-string #"&")]
+         split-params (split params outer-delimiter)]
     (if (= (count split-params) 0)
       acc
-      (let [split-param (split (first split-params) #"=")]
+      (let [split-param (split (first split-params) inner-delimiter)]
         (recur (merge acc {(first split-param) (second split-param)})
                (rest split-params))))))
 
@@ -14,21 +14,12 @@
   (let [split-status-line (split raw-status-line #" ")]
     {:method (split-status-line 0)
      :path (split-status-line 1)
-     :params (parse-params (nth (split (split-status-line 1) #"\?") 1 ""))
+     :params (parse-params (nth (split (split-status-line 1) #"\?") 1 "") #"&" #"=")
      :http-version (split-status-line 2)}))
-
-(defn parse-headers [raw-headers]
-  (loop [acc           {}
-         split-headers (split raw-headers #"\n")]
-    (if (= (count split-headers) 0)
-      {:headers acc}
-      (let [split-header (split (first split-headers) #": ")]
-        (recur (merge acc {(first split-header) (second split-header)})
-               (rest split-headers))))))
 
 (defn parse [raw-request]
   (let [split-request (split raw-request #"\r\n")]
       (merge
         (parse-status-line (nth split-request 0))
-        (parse-headers (nth split-request 1 ""))
+        {:headers (parse-params (nth split-request 1 "") #"\n" #": ")}
         {:body (nth split-request 2 "")})))
