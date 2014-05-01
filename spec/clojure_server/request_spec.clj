ade5fa5e4a3cf1a14ae03fb5a184c492c1b8a619
alexreedhill <ahill1994@student.elgin.edu>
2014-05-01 15:02:39 -0500

    Parse headers and body from request

--- spec/clojure_server/request_spec.clj
+++ spec/clojure_server/request_spec.clj
@@ -4,10 +4,19 @@
 
 (describe "request"
   (it "parses request method"
-      (should= "GET" (get (parse "GET / HTTP/1.1") :method)))
+      (should= "GET" ((parse "GET / HTTP/1.1") :method)))
 
   (it "parses request path"
-      (should= "/" (get (parse "GET / HTTP/1.1") :path)))
+      (should= "/" ((parse "GET / HTTP/1.1") :path)))
 
   (it "parses request http version"
-      (should= "HTTP/1.1" (get (parse "GET / HTTP/1.1") :http-version))))
+      (should= "HTTP/1.1" ((parse "GET / HTTP/1.1") :http-version)))
+
+  (it "parses a single header"
+      (should= ["Foo: Bar"] ((parse "GET / HTTP/1.1\r\nFoo: Bar") :headers)))
+
+  (it "parses multiple headers"
+      (should= ["Foo: Bar" "Boo: Far"] ((parse "GET / HTTP/1.1\r\nFoo: Bar\nBoo: Far") :headers)))
+
+  (it "parses body"
+      (should= "Body" ((parse "GET / HTTP/1.1\r\nHeaders\r\nBody") :body))))
--- src/clojure_server/request.clj
+++ src/clojure_server/request.clj
@@ -1,9 +1,18 @@
 (ns clojure-server.request)
 (use '[clojure.string :only (split)])
 
-(defn parse [raw-request]
-  (let [split-request (split raw-request #"\s+")]
-    {:method (get split-request 0)
-     :path (get split-request 1)
-     :http-version (get split-request 2)}))
+(defn parse-status-line [raw-status-line]
+  (let [split-status-line (split raw-status-line #"\s+")]
+    {:method (split-status-line 0)
+     :path (split-status-line 1)
+     :http-version (split-status-line 2)}))
 
+(defn parse-headers [raw-headers]
+  {:headers (split raw-headers #"\n+")})
+
+(defn parse [raw-request]
+  (let [split-request (split raw-request #"\r\n+")]
+      (merge
+        (parse-status-line (nth split-request 0))
+        (parse-headers (nth split-request 1 ""))
+        {:body (nth split-request 2 "")})))
