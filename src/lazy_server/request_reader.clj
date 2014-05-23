f598dc948bfb5b39cc802366cfaceccf46067e12
alexreedhill <ahill1994@student.elgin.edu>
2014-05-23 10:35:23 -0500

    refactor request reader content length function

--- src/lazy_server/request_reader.clj
+++ src/lazy_server/request_reader.clj
@@ -3,22 +3,19 @@
             [clojure.java.io :refer [reader writer]]
             [clojure.string :refer [blank?]]))
 
-(defn read-until-body [in]
-  (loop [request []]
-    (if (and (blank? (last request)) (> (count request) 1))
-      request
-      (recur (conj request (.readLine in))))))
-
 (defn read-body [in content-length]
   (let [body (char-array content-length)]
     (.read in body 0 content-length)
     (apply str body)))
 
 (defn get-content-length [headers]
-  (try
-    (Integer/parseInt (headers :content-length))
-    (catch NumberFormatException e
-      0)))
+  (read-string (get headers :content-length "0")))
+
+(defn read-until-body [in]
+  (loop [request []]
+    (if (and (blank? (last request)) (> (count request) 1))
+      request
+      (recur (conj request (.readLine in))))))
 
 (defn read-request [client-socket]
   (let [in (reader client-socket)
