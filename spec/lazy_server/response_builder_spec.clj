f3e75fa8c2f82882e73b01cab7143a8016037cbc
alexreedhill <ahill1994@student.elgin.edu>
2014-06-06 23:18:10 -0500

    response builder specs

--- spec/lazy_server/response_builder_spec.clj
+++ spec/lazy_server/response_builder_spec.clj
@@ -1,3 +1,13 @@
 (ns lazy-server.response-builder-spec
   (:require [lazy-server.response-builder :refer :all]
             [speclj.core :refer :all]))
+
+(describe "response builder"
+
+  (it "builds 200 OK response"
+    (should= (build {:path "/"} {:code 200 :body ""})
+      "HTTP/1.1 200 OK\r\n\n"))
+
+  (it "builds 404 Not Found response"
+    (should= (build {:path "/foo"} {:code 404 :body "Oops! There's nothing here"})
+                    "HTTP/1.1 404 Not Found\r\n\nOops! There's nothing here")))
