5b5ce28cefe0fa543df2d105d2502cccf28400a5
alexreedhill <ahill1994@student.elgin.edu>
2014-06-08 13:08:48 -0500

    file interactor reads file, end to end test

--- spec/lazy_server/file_interactor_spec.clj
+++ spec/lazy_server/file_interactor_spec.clj
@@ -1,6 +1,13 @@
 (ns lazy-server.file-interactor-spec
   (:require [lazy-server.file-interactor :refer :all]
-            [clojure.java.io :refer [writer]]
+            [clojure.string :refer [trim]]
+            [clojure.java.io :refer [writer delete-file]]
             [speclj.core :refer :all]))
 
-(describe "file interactor")
+(describe "file interactor"
+  (it "writes to file and reads it"
+    (write-to-file "resources/test-file.txt" "test content")
+    (let [file-contents (apply str (map char (read-file "resources/test-file.txt")))]
+      (should= "test content" (trim file-contents)))
+    (delete-file "resources/test-file.txt")
+    (should-throw (read-file "resources/test-file.txt"))))
--- src/lazy_server/file_interactor.clj
+++ src/lazy_server/file_interactor.clj
@@ -1,10 +1,12 @@
 (ns lazy-server.file-interactor
   (:require [clojure.string :refer [trim]]
-            [clojure.contrib.io :refer [to-byte-array]]
-            [clojure.java.io :refer [writer]]))
+            [clojure.java.io :refer [input-stream writer]]))
 
-(defn read-file [path]
-  (to-byte-array path))
+(defn read-file [file]
+  (with-open [reader (input-stream file)]
+    (let [buffer (byte-array (.length file))]
+      (.read reader buffer)
+      buffer)))
 
 (defn write-to-file [path content]
   (with-open [w (writer path)]
