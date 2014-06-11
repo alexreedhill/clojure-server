113f30794010d9cfdd2b67a3f036e13822425f6e
alexreedhill <ahill1994@student.elgin.edu>
2014-06-11 15:57:17 -0500

    method not allowed plays nicely with four-oh-four
    lower level specs to verify router behavior

--- spec/lazy_server/router_spec.clj
+++ spec/lazy_server/router_spec.clj
@@ -22,10 +22,6 @@
       (should= "HTTP/1.1 200 OK\r\n\n/resource response body"
         (bytes-to-string (get-router {:method "GET" :path "/resource"}))))
 
-    (it "doesn't route unkown method"
-      (should= "HTTP/1.1 404 Not Found\r\n\nSorry, there's nothing here!"
-        (bytes-to-string (get-router {:method "POST" :path "/"}))))
-
     (it "doesn't route unkown path"
       (should= "HTTP/1.1 404 Not Found\r\n\nSorry, there's nothing here!"
         (bytes-to-string (get-router {:method "GET" :path "/foobar"})))))
@@ -58,7 +54,67 @@
         (bytes-to-string (put-router {:method "PUT" :path "/form"})))))
 
   (context "method not allowed"
-    (it "routes method not allowed"
+    (it "method not allowed returns response"
       (should= "HTTP/1.1 405 Method Not Allowed\r\nAllow: GET\r\n\n"
-        (bytes-to-string (check-method-not-allowed {:method "POST" :path "/"} ((GET "/" {:code 200} request))))))))
+        (bytes-to-string (method-not-allowed {:method "POST" :path "/"} request ((GET "/" {:code 200}))))))
+
+    (it "routes method not allowed without four-oh-four defined"
+      (defrouter not-allowed-router request
+        (GET "/" {:code 200})
+        (POST "/" {:code 200}))
+      (should= "HTTP/1.1 405 Method Not Allowed\r\nAllow: GET,POST\r\n\n"
+        (bytes-to-string (not-allowed-router {:method "PUT" :path "/"}))))
+
+    (context "not found"
+      (before-all
+        (defrouter not-allowed-router request
+          (GET "/" {:code 200})
+          (POST "/" {:code 200})
+          (four-oh-four "Sorry, there's nothing here!")))
+
+      (it "routes method not allowed with four-oh-four defined"
+        (should= "HTTP/1.1 405 Method Not Allowed\r\nAllow: GET,POST\r\n\n"
+          (bytes-to-string (not-allowed-router {:method "PUT" :path "/"}))))
+
+      (it "routes to four-oh-four if no path or method matches request"
+        (should= "HTTP/1.1 404 Not Found\r\n\nSorry, there's nothing here!"
+          (bytes-to-string (not-allowed-router {:method "GET" :path "/foobar"}))))))
+
+  (context "matching"
+    (context "path"
+      (it "matches path"
+        (should= true (path-matches? {:path "/"} "/")))
+
+      (it "doesn't match different paths"
+        (should= false (path-matches? {:path "/foobar"} "/"))))
+
+    (context "method"
+      (it "matches method"
+        (should= true (method-matches? {:method "GET"} "GET")))
+
+      (it "doesn't match different methods"
+        (should= false (method-matches? {:method "PUT"} "POST"))))
+
+    (context "request"
+      (it "matches request with route"
+        (should= true (request-matches? {:path "/" :method "GET"} "/" "GET")))
+
+      (it "doesn't match request with route if different method"
+        (should= false (request-matches? {:path "/" :method "POST"} "/" "PUT")))
+
+      (it "doesn't match request with route if different path"
+        (should= false (request-matches? {:path "/" :method "POST"} "/foobar" "POST")))))
+
+  (context "last-route?"
+    (it "determines last route is false when only one route is defined"
+      (let [routes '(route)]
+        (should= false (last-route? routes routes))))
+
+    (it "determines last route is false with more than one route left"
+      (let [routes '(route1 route2)]
+        (should= false (last-route? routes routes))))
+
+    (it "determines last route is true if there is only one route left"
+      (let [routes '(route1 route2)]
+        (should= true (last-route? (list (last routes)) routes))))))
 
--- src/lazy_server/router.clj
+++ src/lazy_server/router.clj
@@ -15,11 +15,12 @@
      (if (request-matches? request# ~path ~method)
        (~handler-fn request#))))
 
-(defmacro check-method-not-allowed [request routes]
+(defmacro method-not-allowed [request request-sym routes]
   `(loop [routes# '~routes
           allowed#     []]
      (cond
-       (= (count routes#) 0) (build ~request (method-not-allowed-response allowed#))
+       (and (= (count routes#) 0) (> (count allowed#) 0)) (build ~request (method-not-allowed-response allowed#))
+       (= (count routes#) 0) ((eval (concat (last '~routes) '(~request-sym))) ~request)
        (path-matches? ~request (second (first routes#))) (recur (rest routes#) (conj allowed# (str (first (first routes#)))))
        :else (recur (rest routes#) allowed#))))
 
@@ -43,11 +44,14 @@
   `(let [handler# (fn [~request-sym] (build ~request-sym {:code 404 :body ~body}))]
      (fn [request#] (handler# request#))))
 
+(defn last-route? [routes-left total-routes]
+  (and (= (count routes-left) 1) (> (count total-routes) 1)))
+
 (defmacro defrouter [router-name request-sym & routes]
   `(defn ~router-name [request#]
      (loop [routes# '~routes]
        (let [response# ((eval (concat (first routes#) '(~request-sym))) request#)]
          (cond
+           (last-route? routes# '~routes) (method-not-allowed request# ~request-sym ~routes)
            (not (nil? response#)) response#
-           (= (count routes#) 1) (check-method-not-allowed request# '~routes)
            :else (recur (rest routes#)))))))
